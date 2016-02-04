5217563c5a0790490f49f07991f0717f8d628b1d
Alistair McKinlay <github@10people.co.uk>
2016-02-04 09:40:26 +0000

    Add bash display gist

---
+++ gists/bash/display.sh
@@ -0,0 +1,10 @@
+# Stuff for bash scripts
+
+# Get the vertical width
+VW=$(tput cols) 
+
+# Right align something
+LEFTTEXT="This will show on the left"
+RIGHTTEXT="This is on the right"
+TABWIDTH=$((VW-${LEFTTEXT}))
+printf "%s%${TABWIDTH}s%s" "$LEFTTEXT" "$RIGHTTEXT"
