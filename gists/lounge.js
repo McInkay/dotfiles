fee562c0f63af4fbcdb0f6faca4af814b51850f0
Alistair McKinlay <github@10people.co.uk>
2016-03-10 15:18:10 +0000

    Add some stuff for textarea and copying

--- gists/lounge.js
+++ gists/lounge.js
@@ -1,4 +1,8 @@
 
+ /******
+  * Text area
+  * ******/
+
 // This should allow enter to send and shift-enter to do newline
 $('#mytextarea').keypress(function(e){
   if(e.keyCode == 13 && !e.shiftKey) {
@@ -6,3 +10,48 @@ $('#mytextarea').keypress(function(e){
    this.form.submit();
   }
 });
+
+
+/* Some CSS
+
+#form {
+  height: 96px; // This will probably have to expand based on the number of lines
+  z-index: 1;
+}
+
+#submit {
+  height: 100%;
+}
+
+#input {
+  rows: 1;
+  padding-left: 92px;
+  resize: none;
+  padding-top: 7px;
+  padding-bottom: 7px;
+  height: 200%;
+}
+*/
+
+/*******
+ * Copy with "<"
+ * http://jsfiddle.net/m56af0je/104/
+ * ******/
+ 
+ function addLink(event) {
+	event.preventDefault();
+
+  var selection = window.getSelection(),
+      copytext = "";
+        
+  var texts = selection.toString().split(" ");
+  copytext = texts[0] + " <" + texts[1] + "> ";
+  texts.splice(0, 2);
+  copytext += texts.join(" ");
+
+	if (event.clipboardData) {
+		event.clipboardData.setData('Text', copytext);
+	}
+}
+
+document.addEventListener('copy', addLink);
