bf41acc44ad9bc8ef0f97577d54265b24639ae5a
Alistair McKinlay <github@10people.co.uk>
2016-04-08 09:31:06 +0200

    Update git check to be a node scipt.

---
+++ bin/libs/shell.js
@@ -0,0 +1,22 @@
+require("shelljs/global");
+
+/**
+ *
+ */
+global.runAndGetOutput = function(command, opts) {
+  const silent = (opts && opts.silent) || true;
+  const stripNewLines = (opts && opts.stripNewLines) || true;
+  const output = exec(command, {silent: silent}).output;
+  return stripNewLines ? output.replace("\n", "") : output;
+}
+
+/**
+ *
+ */
+global.isDirectory = function(path) {
+  return test('-d', path);
+}
+
+global.isGitRepo = function(path) {
+  return isDirectory(path + "/.git");
+}
