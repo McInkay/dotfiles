55a2a6f760230f26eeb3b2fff669f707a2d1e696
Alistair McKinlay <alistair@10people.co.uk>
2017-07-05 15:28:45 +0100

    Add zshrc

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -8,7 +8,7 @@ module.exports = {
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: '"Droid Sans Mono for powerline", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.8)',
@@ -96,7 +96,7 @@ module.exports = {
     // bellSoundURL: 'http://example.com/bell.mp3',
 
     // for advanced config flags please refer to https://hyper.is/#cfg
-		hyperRun: ['hyperlayout']
+		hyperRun: ['cd ~/dev', 'hyperlayout']
   },
 
   // a list of plugins to fetch and install from npm
